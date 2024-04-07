f9058c5c6ef5fbbd4c3c54798b51b44d1f10f460
Jiho <yoho4613@gmail.com>
2024-04-07 12:55:04 +1200

    termsandcondition.txt file added

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,36 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import '../styles/App.css';
-import Footer from '../Components/Footer.tsx';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+import Footer from "../Components/Footer.tsx";
 
 function TermsAndConditions() {
-    const [tcContent, setTcContent] = useState('');
+  const [tcContent, setTcContent] = useState("");
 
-    useEffect(()=>{
-        fetch('termsAndConditionsText.txt')
-        .then(response=>response.text())
-        .then(text=>setTcContent(text))
-        .catch(error=>console.error("Error fetching terms: ", error));
-    })
+  useEffect(() => {
+    fetch("termsAndConditionsText.txt")
+      .then((response) => response.text())
+      .then((text) => setTcContent(text))
+      .catch((error) => console.error("Error fetching terms: ", error));
+  });
 
   return (
     <div className="Site-background">
+      <div className="Site-ScrollContainer">
+        <div className="Site-title">Terms and Conditions</div>
 
-      <div className='Site-ScrollContainer'>
-      <div className="Site-title">
-        Terms and Conditions
-      </div>   
-
-      <div className="Site-TermsAndConditions">
-          {tcContent.split('\n').map((line, index) => (
+        <div className="Site-TermsAndConditions">
+          {tcContent.split("\n").map((line, index) => (
             <React.Fragment key={index}>
               {line}
               <br />
             </React.Fragment>
           ))}
         </div>
-
       </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 }
