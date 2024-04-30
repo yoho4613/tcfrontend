9250e73a32b1647061be2d02f64a40078ead7f52
Jiho <yoho4613@gmail.com>
2024-04-30 20:19:26 +1200

    board page init

---
+++ src/config/type.d.ts
@@ -0,0 +1,9 @@
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+  description: string;
+  image: string;
+  createdAt: string;
+  authorId: string;
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage.tsx';
-import TermsAndConditions from './TermsAndConditions.tsx';
-import NotFoundPage from './NotFoundPage.tsx'; 
-import ContactUs from './ContactUs.tsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage.tsx";
+import TermsAndConditions from "./TermsAndConditions.tsx";
+import NotFoundPage from "./NotFoundPage.tsx";
+import ContactUs from "./ContactUs.tsx";
+import BoardPage from "./BoardPage.tsx";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" Component={LandingPage} />
         <Route path="/TermsAndConditions" Component={TermsAndConditions} />
-        <Route path="/ContactUs" Component={ContactUs}/>
+        <Route path="/ContactUs" Component={ContactUs} />
+        <Route path="/board" Component={BoardPage} />
         <Route Component={NotFoundPage} />
       </Routes>
     </Router>
